17e887c8b9a4aca8638b65ef5bdb978f541584a3
Faeze <faezeahmadi7173@gmail.com>
2023-09-07 16:09:08 +0330

    feat/faeze/js-10/end-of-practice-video-js12

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,72 @@
 import "./style.css";
-
 const app = document.querySelector<HTMLDivElement>("#app");
-app!.innerHTML = `<h1 class="bg-pink-600">hello from vite!</h1>`;
 
-const h1 = document.createElement("h1");
-h1.classList.add("bg-pink-600");
+// app!.innerHTML = `<h1 class="bg-pink-600">hello from vite!</h1>`;
 
-const span = document.createElement("span");
-span.innerHTML = "hello vite";
+// const h1 = document.createElement("h1");
+// h1.classList.add("bg-pink-600");
 
-h1.appendChild(span);
+// const span = document.createElement("span");
+// span.innerHTML = "hello vite";
 
-app?.appendChild(h1);
+// h1.appendChild(span);
 
-const input = document.createElement("input");
-input.classList.add("bg-red-600")
+// app?.appendChild(h1);
 
-function myFunction() {
-  var x = document.createElement("INPUT");
-  x.setAttribute("type", "text");
-  x.setAttribute("value", "Hello World!");
-  document.body.appendChild(x);
-}
+// const input = document.createElement("input");
+// input.classList.add("bg-red-600")
 
-myFunction()
+// function myFunction() {
+//   var x = document.createElement("INPUT");
+//   x.setAttribute("type", "text");
+//   x.setAttribute("value", "Hello World!");
+//   document.body.appendChild(x);
+// }
 
-var x = document.createElement("INPUT");
-x.setAttribute("type", "text");
+// myFunction()
 
-app?.appendChild(x);
+// var x = document.createElement("INPUT");
+// x.setAttribute("type", "text");
+
+// app?.appendChild(x);
 
 type BoxProps = {
   element: string;
-  attr?: Element;
+  attr?: any;
   children?: string | string[] | Element | Element[];
 };
 
 const Box = (props: BoxProps) => {
+  console.log(props.attr);
   const el = document.createElement(props.element);
-  el.append(props.children)
+  
+  if (props.attr)
+    Object.keys(props.attr).forEach((key: string) => {
+      el[key] = props.attr[key];
+  })
+
+  if (!Array.isArray(props.children)) el.append(props.children!)
+  else el.append(...props.children)
   return el
 };
 
-console.log(Box({element: "h1", children: "sakcvahk"}))
+console.log(Box({element: "h1", children: "sakcvahk"}));
+app!.append(Box({element: "h1", children: "mehdi malkami"}));
+app!.append(Box({element: "mark", children: "faeze ahmadi"}));
+app!.append(Box({element: "h1", children: ["faeze ahmadi", " ", "loves", " ", "mehdi malkami"]}));
+
+app!.append(
+  Box({
+    element: "ul",
+    attr: {
+      className: "bg-red-700",
+      id: "1000",
+      style: "red",
+    },
+    children: [
+      Box({element: "li", children: "1"}),
+      Box({element: "li", children: "2"}),
+      Box({element: "li", children: "3"})
+    ]
+  })
+)
