2029397865c11fa8e1aff2295b1c63953b5f3f8e
Faeze <faezeahmadi7173@gmail.com>
2023-09-07 14:33:25 +0330

    feat/faeze/js-12/create-element

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,16 @@ import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 app!.innerHTML = `<h1 class="bg-pink-600">hello from vite!</h1>`;
+
+const h1 = document.createElement("h1");
+h1.classList.add("bg-pink-600");
+
+const span = document.createElement("span");
+span.innerHTML = "hello vite";
+
+h1.appendChild(span);
+
+app?.appendChild(h1);
+
+// const input = document.createElement("input");
+// input.classList.add("bg-red-600")
